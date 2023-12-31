dabf87e9e436ff3b94125c26721af18bd3bd0746
PeytenBevill <redsage00@gmail.com>
2023-12-31 17:16:21 -0600

    problems with usb logic

--- src/components/Inventory/Inventory.jsx
+++ src/components/Inventory/Inventory.jsx
@@ -7,49 +7,38 @@ const Inventory = () => {
   const [isScannerConnected, setIsScannerConnected] = useState(false);
 
   useEffect(() => {
-    const startScanner = async () => {
+    const checkScanner = async () => {
       try {
-        // Check if the scanner is connected
-        const scannerInfo = await axios.get(
-          "https://second-inventory-backend.onrender.com/check-scanner"
-        );
+        const scannerInfo = await axios.get('https://second-inventory-backend.onrender.com/check-scanner');
+
         if (scannerInfo.data.isScannerConnected) {
-          console.log("Scanner connected!");
+          console.log('Scanner connected!');
           setIsScannerConnected(true);
         } else {
-          console.log("Scanner not connected.");
+          console.log('Scanner not connected.');
           setIsScannerConnected(false);
         }
       } catch (error) {
-        console.error("Error checking scanner:", error.message);
+        console.error('Error checking scanner:', error.message);
       }
     };
 
     // Check scanner status when the component mounts
-    startScanner();
-
-    // Listen for USB device changes
-    usb.on("add", startScanner);
-    usb.on("remove", startScanner);
-
-    // Clean up the USB event listeners when the component unmounts
-    return () => {
-      usb.removeAllListeners("add");
-      usb.removeAllListeners("remove");
-    };
+    checkScanner();
   }, []);
 
   const handleStartScan = async () => {
     try {
       // Start the scan on the server
-      await axios.post("https://second-inventory-backend.onrender.com/start-scan");
+      await axios.post('https://second-inventory-backend.onrender.com/start-scan');
 
       // Display a message or handle the response as needed
-      console.log("Scan started");
+      console.log('Scan started');
     } catch (error) {
-      console.error("Error starting scan:", error.message);
+      console.error('Error starting scan:', error.message);
     }
   };
+
 
   return (
     <div className="inventory-page">
